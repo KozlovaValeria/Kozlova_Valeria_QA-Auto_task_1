93e1bf8c1c81f1fd7239c9490559a148d07f6ff2
Валерия Романовская <valeria.romanovskaya96@gmail.com>
2022-12-21 14:46:01 +0100

    Calculated average, min, max values.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = new int[10] {8,9,2,8,10,7,7,5,6,10};
+float result = 0f;
+int min = array[0];
+int max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    result = result + array[i];
+}
+result = result/array.Length;
+Console.WriteLine(result);
+
+for (int i = 1; i < 10; i++)
+{
+    if(min > array[i])
+    {
+        min = array[i];
+    }
+    if(max < array[i])
+    { 
+        max = array[i];
+    }
+}
+Console.WriteLine(max);
+Console.WriteLine(min); 
